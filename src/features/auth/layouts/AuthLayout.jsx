03d93398df82958121c0ac37fe9da4b0ccee88b2
JosuaIF23 <142556166+JosuaIF23@users.noreply.github.com>
2025-09-24 20:58:25 +0700

    Commit 2 done AuthApi api.js

---
+++ src/features/auth/layouts/AuthLayout.jsx
@@ -0,0 +1,3 @@
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, Outlet, useLocation, useNavigate } from "react-router-dom";
+import apiHelper from "../../../helpers/apiHelper";
---
+++ src/helpers/apiHelper.js
@@ -0,0 +1,38 @@
+const apiHelper = (() => {
+  async function fetchData(url, options) {
+    const urlQuery = url.includes("?") ? url.split("?")[1] : "";
+
+    const urlWithoutQuery = url.replace(`?${urlQuery}`, "");
+
+    const fixUrl = urlWithoutQuery.endsWith("/")
+      ? urlWithoutQuery.slice(0, -1)
+      : urlWithoutQuery;
+
+    const fullUrl = fixUrl + (urlQuery ? `?${urlQuery}` : "");
+
+    return fetch(fullUrl, {
+      ...options,
+      mode: "cors",
+      headers: {
+        ...options.headers,
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+    });
+  }
+
+  function putAccessToken(token) {
+    localStorage.setItem("accessToken", token);
+  }
+
+  function getAccessToken() {
+    return localStorage.getItem("accessToken");
+  }
+
+  return {
+    fetchData,
+    putAccessToken,
+    getAccessToken,
+  };
+})();
+
+export default apiHelper;
