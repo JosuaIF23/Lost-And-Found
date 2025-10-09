ee4208ba183c955e4a5650073104197862139066
JosuaIF23 <142556166+JosuaIF23@users.noreply.github.com>
2025-10-09 13:04:36 +0700

    commit has router

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,20 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store"; 
+import store from "./store";
 import "./index.css";
 import "./features/lostfound/resources/custom.css";
+
+HashRouter;
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </StrictMode>
   </Provider>
 );
